4e16c4d63e954fdb79cddd5e4751f4619fd8ec49
Chiknas <nkuknas@gmail.com>
2021-10-20 19:48:29 +0100

    add websocket server

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
-import http from 'http';
+import { WebSocketServer } from 'ws';
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const wss = new WebSocketServer({ port: 3000 });
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    ws.send(`You said: ${message}`);
+  });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  ws.send('I see you!');
 });
