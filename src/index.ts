1cd00433cf5daba481505e56491f55c6e9f4648c
Chiknas <nkuknas@gmail.com>
2021-10-21 19:49:40 +0100

    setup http server to serve static content

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,20 @@ import { handleControls } from './inputhandlers/ControlsHandler/ControlsHandler'
 import { WebSocketServer } from 'ws';
 import { Message } from './types';
 import { Controls } from './inputhandlers/ControlsHandler/types';
+import express from 'express';
+import path from 'path';
 
+// Websocket for the main communication channel
 const wss = new WebSocketServer({ port: 3000 });
-
-const handleMessage = (message: Message): void => {
-  switch (message.type) {
-    case 'controls':
-      handleControls(message as Controls);
-      break;
-  }
-};
-
 wss.on('connection', (ws) => {
+  const handleMessage = (message: Message): void => {
+    switch (message.type) {
+      case 'controls':
+        handleControls(message as Controls);
+        break;
+    }
+  };
+
   ws.on('message', (data) => {
     try {
       const message = JSON.parse(data.toString()) as Message;
@@ -26,3 +28,15 @@ wss.on('connection', (ws) => {
 
   ws.send('I see you!');
 });
+
+// http server to serve the main page
+const serverPort = 80;
+const app = express();
+app.use(express.static(path.join(__dirname, '../dist/static')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/static/index.html'));
+});
+
+app.listen(serverPort, () =>
+  console.log(`Http server started on port ${serverPort}`)
+);
---
+++ src/static/index.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line no-unused-vars
+function alertMe(): string {
+  alert('test2');
+  return 'nikos';
+}
